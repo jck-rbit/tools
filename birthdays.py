a05170cdadfb0aade3454dc1c70f1bd62cb4a18a
jckrbit <jckrbit@proton.me>
2022-08-29 21:32:48 +0100

    Created 'birthdaysFunc.py', an updated version of 'birthday.py' which uses functions and global variables to allow you to repeat the program without having to re-run it.

--- birthdays.py
+++ birthdays.py
@@ -5,10 +5,23 @@ with open('birthday list.txt', 'rb') as og_list:
 
 # taking input
 
-task = input("Would you like to: [add], [remove], [find] a birthday, or [view] the list? ").lower()
+task = input("Would you like to: [add], [remove], [find] a birthday, check for birthdays [today], [check] if someone is in the list or [view] the list? ").lower()
 
 if task == 'view':
     print(birthday_list)
+
+elif task == 'today':
+    current_date = input("Today's date? ").lower()
+    for name, date in birthday_list.items():
+        if current_date == date:
+            print( "It is" , name +"'s birthday today.")
+
+elif task == 'check':
+    name = input("Name? ").lower()
+    if name in birthday_list:
+        print(name +"'s birthday is in the list.")
+    else:
+        print(name+"'s birthday is not in the list.")
 
 elif task == 'add':
     name = input("Name? ").lower()
